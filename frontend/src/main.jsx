f4ca25e0738f81b75ac1c52c05604439dde4f3d9
tyche <jeridpan@addu.edu.ph>
2024-05-08 19:32:46 +0800

    Login System added
    - Added Dynamic Form for login and register
    - Backend and Frontend folders separated
    - Backend not working properly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,56 +1,11 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
-import Products from "./components/Products/Products";
-import Cart from "./components/Cart/Cart";
-import Login from "./components/LoginRegister/Login";
-import Register from "./components/LoginRegister/Register";
-import User from "./components/User/User";
-import Errorpage from "./components/Errors/Errorpage";
+import App from "./App"
 
-const router = createBrowserRouter([
-  
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <Errorpage/>,
-    children: [
-      {
-        path: "",
-        element: <Products />,
-      },
-      {
-        path: "products",
-        element: <Products />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-      {
-        path: "User",
-        element: <User />,
-      },
-    ],
-  },
-  {
-    path:"login",
-    element: <Login />,
-  },
-  {
-    path:"register",
-    element: <Register />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
