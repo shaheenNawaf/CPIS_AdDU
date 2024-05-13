47c47006a882cf59ec975499081a87bea6972a44
tyche <jeridpan@addu.edu.ph>
2024-05-13 13:53:52 +0800

    Backend Added
    - Moved around Folders
    - Register Page added
    - Some frontend files reworked
    - Added some files to frontend
    - Backend added

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+# THIS IS WHERE PATHS ARE HANDLED
+
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path("products/create", views.ProductCreate.as_view(), name="product-create"),
+    path("products/list", views.ProductListView.as_view(), name="product-list"),
+    path("order/", views.OrderItemCreateView.as_view(), name="order-list"),
+]
